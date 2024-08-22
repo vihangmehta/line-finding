af4167021c06d126d7e50c2dc184702ec2f267f0
kalinanedkova <kalina.nedkova@gmail.com>
2024-08-22 14:58:41 -0400

    automatically create region files & line lists

--- mainPASSAGE.py
+++ mainPASSAGE.py
@@ -9,16 +9,39 @@ CODE_DIR = "/Users/knedkova/Work/2024PASSAGE/wisp_analysis"
 OUTPUT_DIR = "/Users/knedkova/Work/2024PASSAGE/output"
 path_to_wisp_data = "/Users/knedkova/Work/2024PASSAGE/data/"
 
-# The field number:
-parno = 28
+#############################################################
+####### You should not need to change anything below. #######
+#############################################################
 
-# To run the code, first run wisp.loop_field_cwt() to create the line list for thr field you're working on
-# Once that is done, that line can be commented out, unless the line list needs to be recreated (e.g. if you're changing fields).
-# Next, run wisp.measure_z_interactive() to fit the spectra.
+parno = input('\033[94m' + "Enter the number of the parallel field you want to analyze.\n> " + '\033[0m')
+# pull out only the number, in case the user entered e.g. "Par1"
+while True:
+    try:
+        parno = int(re.findall(r'\d+', str(parno))[0])
+    except: 
+        parno = input('\033[94m' + "A parallel field number is required. Enter the number of the parallel field you want to analyze.\n> " + '\033[0m')
+        continue
+    else:
+        break
 
 if __name__ == "__main__":
     # move to the directory where you want your outputs.
     os.chdir(OUTPUT_DIR)
-    # wisp.loop_field_cwt(path_to_wisp_data=path_to_wisp_data, path_to_code=CODE_DIR, parno=parno)
+
+    # check if region files exist. If not, run code to create necessary region files
+    regionfiles = glob.glob(path_to_wisp_data + "Par" + str(parno) + "/DATA/*.reg")
+    if len(regionfiles) == 0:
+        print('\033[94m' + "No region files found, creating those for you now."  + '\033[0m')
+        create_regions(parno = parno, path_to_wisp_data = path_to_wisp_data)
+
+    # check if line list exists. If not, run code to create the linelist
+    print(OUTPUT_DIR + "/linelist/Par"+str(parno)+"lines.dat")
+    linelist = glob.glob(OUTPUT_DIR + "/linelist/Par"+str(parno)+"lines.dat")
+    if len(linelist) == 0:
+        print('\033[94m' + "No line list file found, creating the line list for you now." + '\033[0m')
+        wisp.loop_field_cwt(path_to_wisp_data=path_to_wisp_data, path_to_code=CODE_DIR, parno=parno)
+    
+    # run the measure_z_interactive codes
     wisp.measure_z_interactive(path_to_wisp_data=path_to_wisp_data, path_to_code=CODE_DIR, parno=parno)
+
 
