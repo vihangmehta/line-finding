6fd93e188b90dfa44070ed50f0d5d64336205b7e
vihangmehta <mehta.vihang@gmail.com>
2025-01-31 17:06:59 -0800

    more column name fixes

--- passage_analysis/find_cwt.py
+++ passage_analysis/find_cwt.py
@@ -194,7 +194,7 @@ def loop_field_cwt(path_to_data, path_to_code, parno):
         # get spectral data
         # spdata = asc.read(filename, names = ['lambda', 'flux', 'ferror', 'contam', 'zero'])
         spdata = Table.read(filename, format="ascii")
-        spdata.rename_columns(["wave", "error", "zeroth"], ["lambda", "ferror", "zero"])
+        spdata.rename_columns(["wave", "ferr", "zeroth"], ["lambda", "ferror", "zero"])
         trimmed_spec = trim_spec(spdata, None, None, config_pars)
 
         # look up the object in the se catalog and grab the a_image
@@ -254,7 +254,7 @@ def loop_field_cwt(path_to_data, path_to_code, parno):
         print('starting obj id = ', filename)
         # spdata = asc.read(filename, names = ['lambda', 'flux', 'ferror', 'contam', 'zero'])
         spdata = Table.read(filename, format="ascii")
-        spdata.rename_columns(["wave", "error", "zeroth"], ["lambda", "ferror", "zero"])
+        spdata.rename_columns(["wave", "ferr", "zeroth"], ["lambda", "ferror", "zero"])
         trimmed_spec = trim_spec(None, spdata, None, config_pars)
         # beam = float(filename.split('_')[1].split('.')[0])
         beam = float(filename.split('Spectra/Par')[1].split('_')[1].split('.')[0])
@@ -304,7 +304,7 @@ def loop_field_cwt(path_to_data, path_to_code, parno):
         print('starting obj id = ', filename)
         # spdata = asc.read(filename, names = ['lambda', 'flux', 'ferror', 'contam', 'zero'])
         spdata = Table.read(filename, format="ascii")
-        spdata.rename_columns(["wave", "error", "zeroth"], ["lambda", "ferror", "zero"])
+        spdata.rename_columns(["wave", "ferr", "zeroth"], ["lambda", "ferror", "zero"])
         trimmed_spec = trim_spec(None, None, spdata, config_pars)
         # beam = float(filename.split('_')[1].split('.')[0])
         beam = float(filename.split('Spectra/Par')[1].split('_')[1].split('.')[0])
